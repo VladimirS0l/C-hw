ea0518e4cf9f3829786d688ac8db7818e4e571d1
SolarevVladimir <vbnm1993@yandex.ru>
2022-06-28 20:28:48 +0500

    task3 win

--- C#homework2/task3/Program.cs
+++ C#homework2/task3/Program.cs
@@ -4,4 +4,38 @@
 7 -> да
 1 -> нет*/
 
+/*Console.WriteLine("Введите день недели от 1 до 7: ");
+int day = Convert.ToInt32(Console.ReadLine());
+if(day == 6 || day == 7)
+{
+    Console.WriteLine("Выходной день");
+}
+else if(day < 1 || day > 7)
+{
+    Console.WriteLine("Такого дня недели не существует");
+}
+else
+{
+    Console.WriteLine("Рабочий день");
+}*/
 
+Console.WriteLine("Введите день недели от 1 до 7: ");
+int day = Convert.ToInt32(Console.ReadLine());
+
+void week(int day)
+{
+    if(day == 6 || day == 7)
+    {
+        Console.WriteLine("Выходной день");
+    }
+    else if(day < 1 || day > 7)
+    {
+        Console.WriteLine("Такого дня недели не существует");
+    }
+    else
+    {
+    Console.WriteLine("Рабочий день");
+    }
+}
+
+week(day);
