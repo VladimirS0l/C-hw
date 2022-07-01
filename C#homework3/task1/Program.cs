4e8a11854e3d877403257eb5ce8f1203f3700a34
SolarevVladimir <vbnm1993@yandex.ru>
2022-07-01 21:18:53 +0500

    task1

--- C#homework3/task1/Program.cs
+++ C#homework3/task1/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*Напишите программу, которая принимает на вход пятизначное число и проверяет, 
+является ли оно палиндромом.
+14212 -> нет
+12821 -> да
+23432 -> да*/
+
+bool palindrome(int number)
+{
+    int div = 1;
+    while(number / div >= 10)
+    {
+        div *= 10;
+    }
+    while(number != 0)
+    {
+        int left = number / div;
+        int right = number % 10;
+        if(left != right)
+        {
+            Console.WriteLine("Заданное число не является паллиндромом");
+            return false;
+        }
+        number = (number % div) /10;
+        div /= 100;
+    }
+    Console.WriteLine("Заданное число паллиндром");
+    return true;
+}
+    
+palindrome(14212);
+palindrome(12821);
+palindrome(23432);
