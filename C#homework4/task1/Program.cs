de6cd07b42c119bb3efb83575c388c643350ca5d
SolarevVladimir <vbnm1993@yandex.ru>
2022-07-05 21:45:12 +0500

    task1

--- C#homework4/task1/Program.cs
+++ C#homework4/task1/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*Задача 25: Напишите цикл, который принимает на вход два числа (A и B)
+ и возводит число A в натуральную степень B.
+3, 5 -> 243 (3⁵)
+2, 4 -> 16*/
+
+double ab(double a, double b)
+{
+    double result = Math.Pow(a, b);
+    Console.WriteLine(result);
+    return result;
+}
+ab(3, 5);
+ab(2, 4);
