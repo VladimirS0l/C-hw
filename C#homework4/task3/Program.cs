e6091c68c16555824d960c5bca554bc28c2f19d6
SolarevVladimir <vbnm1993@yandex.ru>
2022-07-07 20:49:33 +0500

    task4

--- C#homework4/task3/Program.cs
+++ C#homework4/task3/Program.cs
@@ -1,33 +1,15 @@
 ﻿/*Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
 6, 1, 33 -> [6, 1, 33]*/
-/*
-void arrayOut(int [] array)
-{
-    
-    Console.Write(array[' '] );
-    return result;
-}
-
-arrayOut(1, 2, 5, 7, 19);*/
 
 
-
-
-
-
-// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
-// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
-// 6, 1, 33 -> [6, 1, 33]
-
-Console.WriteLine($"\nЗадача 29. Ряд чисел преобразует в массив");
 Console.Write("Введите ряд чисел, разделенных запятой : ");
 string? seriesOfNumbers = Console.ReadLine();
 
-seriesOfNumbers = seriesOfNumbers + ",";    // дополнительня запятая для обозначения конца строки
+seriesOfNumbers = seriesOfNumbers + ",";
 
-// функция удаления пробелов из строки 
-string RemovingSpaces (string series){
+string RemovingSpaces (string series)
+{
   string seriesNew = "";
   for (int i = 0; i < series.Length; i++)
   {
@@ -39,54 +21,54 @@ string RemovingSpaces (string series){
   return seriesNew;
 }
 
-//  функция  проверки на правильность ввода 
-void СheckNumber2 (int  series){
-
-      if (series == '0'||series == '1'||series == '2'
-      ||series == '3'||series == '4'||series == '5'||series == '6'
-      ||series == '7'||series == '8'||series == '9'||series == ','
-      ||series == '-')
-      {
-      }
-        else {
-          Console.WriteLine($"Ошибка ввода  символа. Вводи цифры.");
-
-      }
+void СheckNumber2 (int  series)
+{
+  if (series == '0'||series == '1'||series == '2'
+  ||series == '3'||series == '4'||series == '5'||series == '6'
+  ||series == '7'||series == '8'||series == '9'||series == ','
+  ||series == '-')
+  {
+  }
+  else
+  {
+    Console.WriteLine($"Ошибка ввода  символа. Попробуй еще раз.");
+  }
 }
 
-// функция  создания и заполнения массива из строки
-int[] ArrayOfNumbers(string seriesNew){ 
-
-  int[] arrayOfNumbers = new int[1];    // инициализация массива из 1 элемента
-
-  int j =0;
-
-  for (int i = 0; i < seriesNew.Length; i++){
+int[] ArrayOfNumbers(string seriesNew)
+{ 
+  int[] arrayOfNumbers = new int[1];
+  int j = 0;
+  for (int i = 0; i < seriesNew.Length; i++)
+  {
     string seriesNew1 = "";
-
-    while (seriesNew[i] != ',' && i < seriesNew.Length){
+    while (seriesNew[i] != ',' && i < seriesNew.Length)
+    {
       seriesNew1 += seriesNew[i];
       СheckNumber2(seriesNew[i]);
       i++;
     }
-    arrayOfNumbers[j] = Convert.ToInt32(seriesNew1);    // заполняет массив значениями из строки
-    if (i < seriesNew.Length-1){
-      arrayOfNumbers = arrayOfNumbers.Concat(new int[] {0}).ToArray();    // добавляет новый нулевой элемент в конец массива
+    arrayOfNumbers[j] = Convert.ToInt32(seriesNew1); 
+    if (i < seriesNew.Length-1)
+    {
+      arrayOfNumbers = arrayOfNumbers.Concat(new int[] {0}).ToArray();
     }
     j++;
   }
   return arrayOfNumbers;
 }
 
-// функция  вывода массива на печать 
-void PrintArry(int[] coll){
+void PrintArray(int[] coll)
+{
   int count = coll.Length;
   int index = 0;
   Console.Write("[");
-  while(index < count){
+  while(index < count)
+  {
     Console.Write(coll[index]);
     index++;
-    if (index < count){
+    if (index < count)
+    {
       Console.Write(", ");
     }
   }
@@ -94,7 +76,5 @@ void PrintArry(int[] coll){
 } 
 
 string seriesNew = RemovingSpaces(seriesOfNumbers);
-
 int[] arrayOfNumbers =  ArrayOfNumbers(seriesNew);
-
-PrintArry(arrayOfNumbers);
+PrintArray(arrayOfNumbers);
