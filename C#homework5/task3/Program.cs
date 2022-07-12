50c641b81968cb762daa27c9145e70596c492745
SolarevVladimir <vbnm1993@yandex.ru>
2022-07-12 22:21:16 +0500

    5.3.2

--- C#homework5/task3/Program.cs
+++ C#homework5/task3/Program.cs
@@ -5,14 +5,16 @@
 int length = 5;
 int[] array = new int[length];
 Random rnd = new Random();
-int minNum = 99; //почему то, когда стоит значение array[0], он всегда выводит minNum = 0
-int maxNum = array[0];
+int minNum = 0;
+int maxNum = 0;
 Console.Write("[");
 
 for(int i = 0; i < length; i++)
 {
     array[i] = rnd.Next(1, 99);
     Console.Write(array[i] + " ");
+    minNum = array[0];
+    maxNum = array[0];
     if(minNum > array[i])
     {
         minNum = array[i];
