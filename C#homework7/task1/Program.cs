105cfd98ae0f36057798df8b89fcd2f80f09ce91
SolarevVladimir <vbnm1993@yandex.ru>
2022-07-15 22:42:34 +0500

    hw7.1 win

--- C#homework7/task1/Program.cs
+++ C#homework7/task1/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+// m = 3, n = 4.
+// 0,5 7 -2 -0,2
+// 1 -3,3 8 -9,9
+// 8 7,8 -7,1 9
+
+Console.WriteLine("Задайте число m в двумерном массиве: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Задайте число n в двумерном массиве: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[,] array = new int[m, n];
+
+Random rnd = new Random();
+for (int i = 0; i < m; i++)
+{
+    for (int j = 0; j < n; j++)
+    {
+        array[i, j] = rnd.Next(-100, 100);
+        Console.Write(array[i, j] + " ");
+    }
+    Console.WriteLine();
+}
