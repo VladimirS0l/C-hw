e1642a7594a197bc34b6939e3101b7adb2c316da
SolarevVladimir <vbnm1993@yandex.ru>
2022-07-21 23:29:51 +0500

    task4.1

--- C#homework8/task4/Program.cs
+++ C#homework8/task4/Program.cs
@@ -3,3 +3,27 @@
 // массив размером 2 x 2 x 2
 // 12(0,0,0) 22(0,0,1)
 // 45(1,0,0) 53(1,0,1)
+
+int r = 2;
+int c = 2;
+int d = 2;
+
+int[,,] array = new int[r, c, d];
+
+Random random = new Random();
+
+for (int i = 0; i < r; i++)
+{
+    for (int j = 0; j < c; j++)
+    {
+        for (int k = 0; k < d; k++)
+        {
+            array[i, j, k] = random.Next(10, 99);
+            Console.Write($"{array[i, j, k]} ({i}, {j}, {k})\t");
+                    
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
