75ff6231852bc1be033a6c202bce2c399120e990
SolarevVladimir <vbnm1993@yandex.ru>
2022-07-21 21:48:35 +0500

    task3 win

--- C#homework8/task3/Program.cs
+++ C#homework8/task3/Program.cs
@@ -15,4 +15,55 @@
 // 56 8 4 24
 // 10 6 24 49
 
+int rows = 4;
+int columns = 4;
+int[,] arrayFirst = GenerateArray(rows, columns);
+int[,] arraySecond = GenerateArray(rows, columns);
+int[,] array = new int[rows, columns];
+
+Console.WriteLine("Массив номер один: ");
+PrintArray(arrayFirst);
+Console.WriteLine("Массив номер два: ");
+PrintArray(arraySecond);
+Console.WriteLine("Произведение двух матриц: ");
+CompositionArray(arrayFirst, arraySecond, array);
+PrintArray(array);
+
+int[,] GenerateArray(int m, int n)
+{
+    int[,] arr = new int[m, n];
+    Random random = new Random();
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            arr[i, j] = random.Next(1, 9);
+        }
+    }
+    return arr;
+}
+void PrintArray(int[,] arrayPrint)
+{
+    for (int i = 0; i < arrayPrint.GetLength(0); i++)
+    {
+        for (int j = 0; j < arrayPrint.GetLength(1); j++)
+        {
+            Console.Write(arrayPrint[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void CompositionArray(int[,] arrayFirst, int[,] arraySecond, int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = arrayFirst[i, j] * arraySecond[i, j];
+        }
+    }
+}
+
+
 
